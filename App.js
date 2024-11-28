4a21b002d2eb604e506723b15e9efff136685ac8
Rakib Islam <rakib13x@gmail.com>
2024-11-28 14:10:20 +0600

    image added

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require(// @ts-ignore
+        "./assets/favicon.png")}
+        style={{
+          width: 150,
+          height: 150,
+          borderRadius: 150,
+          borderWidth: 5,
+          borderColor: "black",
+        }}
+      />
+      <Text style={{ fontWeight: "bold", fontSize: 20 }}>Rakib Islam</Text>
+      <Text style={{}}>is an unemployed.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
